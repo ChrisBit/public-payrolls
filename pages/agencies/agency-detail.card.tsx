f9ca67075cc377be1588a3c67d3b728f6abfefe1
Robert Dobyns <robertddobyns@gmail.com>
2021-08-08 23:43:51 -0500

    Cleaned up title and agency details

--- pages/agencies/agency-detail.card.tsx
+++ pages/agencies/agency-detail.card.tsx
@@ -3,18 +3,17 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import {CardActions, CardHeader, Link} from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+    backgroundColor: '#f0f0f0'
   },
   bullet: {
     display: "inline-block",
     margin: "0 2px",
     transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
   },
   pos: {
     marginBottom: 12,
@@ -22,26 +21,29 @@ const useStyles = makeStyles({
 });
 
 export default function AgencyDetailCard({ agency }) {
-  const { employeeCount, name, topPay, medianPay, year } = agency;
+  const { employeeCount, name, topPay, medianPay} = agency;
   const classes = useStyles();
   return (
     <>
       <Card className={classes.root} variant="outlined">
         <CardContent>
-          <Typography variant="h5" component="h2">
-            {name}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            ({year})
-          </Typography>
+          <CardHeader
+            title={`About ${name}`}
+            subheader={`Serving ${employeeCount} people in Nebraska`}
+          />
           <Typography variant="body2" component="p">
-            Top Pay: {topPay}
-            <br />
-            Median Pay: {medianPay}
-            <br />
-            employees: {employeeCount}
+            Total Payroll: TBD
+            <hr/>
+            Top Pay: ${topPay}
+            <hr/>
+            Median Pay: ${medianPay}
           </Typography>
         </CardContent>
+        <CardActions>
+          <Link href={'#'} variant={'body2'}>
+            Agency Website
+          </Link>
+        </CardActions>
       </Card>
     </>
   );
