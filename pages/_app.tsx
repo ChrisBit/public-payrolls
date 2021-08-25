b83c7376042bf068aa68ac2093d82484cf54791a
dodgees <dodgees2@gmail.com>
2021-08-25 06:07:48 -0500

    Fixing issue with mobile not filling full screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import React from "react";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <SearchAppBar />
       <Component {...pageProps} />
     </>
