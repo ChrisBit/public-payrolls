2841ab1c7b77166ae777d31e66feeed5b4bc51d7
dodgees <dodgees2@gmail.com>
2021-09-07 20:29:56 -0500

    Fix linting errors to satisfy Vercel

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import {
   AppBar,
-  Box,
   Container,
   Grid,
   Link,
   Toolbar,
   Typography,
 } from "@material-ui/core";
+import Image from "next/image";
+import flatwaterLogo from "../public/flatwater-logo.jpeg";
+import plattLogo from "../public/platte-logo.svg";
+import codeForNebLogo from "../public/code-for-nebraska-logo.jpeg";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
@@ -16,6 +19,9 @@ const useStyles = makeStyles({
   },
   logo: {
     width: 175,
+    padding: 15,
+  },
+  imageContainer: {
     padding: 15,
   },
 });
@@ -29,22 +35,35 @@ export default function Footer() {
           <div style={{ width: "100%" }}>
             <Grid container>
               <Grid item sm={12} lg={4} className={classes.footerItem}>
-                <Link href="https://flatwaterfreepress.org/">
-                  <img className={classes.logo} src="/flatwater-logo.jpeg" />
-                </Link>
+                <div className={classes.imageContainer}>
+                  <Link href="https://flatwaterfreepress.org/">
+                    <Image
+                      src={flatwaterLogo}
+                      alt="Flatwater Free Press logo"
+                      width={175}
+                      height={109}
+                    />
+                  </Link>
+                </div>
               </Grid>
               <Grid item sm={12} lg={4} className={classes.footerItem}>
-                <Link href="https://www.codefornebraska.org">
-                  <img
-                    className={classes.logo}
-                    src="/code-for-nebraska-logo.jpeg"
-                  />
-                </Link>
+                <div className={classes.imageContainer}>
+                  <Link href="https://www.codefornebraska.org">
+                    <Image
+                      src={codeForNebLogo}
+                      alt="Code for Nebraska logo"
+                      width={175}
+                      height={86}
+                    />
+                  </Link>
+                </div>
               </Grid>
               <Grid item sm={12} lg={4} className={classes.footerItem}>
-                <Link href="https://platteinstitute.org/">
-                  <img className={classes.logo} src="/platte-logo.svg" />
-                </Link>
+                <div className={classes.imageContainer}>
+                  <Link href="https://platteinstitute.org/">
+                    <Image src={plattLogo} alt="Platte Institute logo" />
+                  </Link>
+                </div>
               </Grid>
               <Grid item sm={12} className={classes.footerItem}>
                 <Typography>
--- pages/_document.js
+++ pages/_document.js
@@ -33,7 +33,7 @@ export default class MyDocument extends Document {
 
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with server-side generation (SSG).
-MyDocument.getInitialProps = async (ctx) => {
+MyDocument.getInitialProps = async function getProps(ctx) {
   // Resolution order
   //
   // On the server:
