d5dace7f7edaf0103f8df6ad0dfa4eacf04b637d
dodgees <dodgees2@gmail.com>
2021-10-01 09:24:08 -0500

    Put scatter plot in a card

--- components/SalaryScatterChart.tsx
+++ components/SalaryScatterChart.tsx
@@ -10,6 +10,7 @@ import {
 } from "recharts";
 import moment from "moment";
 import { formatCurrency } from "../utils/utils";
+import { Card } from "@material-ui/core";
 
 export default function SalaryScatterChart({
   employeeList,
@@ -29,50 +30,52 @@ export default function SalaryScatterChart({
       : 0,
   };
   return (
-    <ResponsiveContainer height={400} width="95%" minWidth="375px">
-      <ScatterChart
-        height={400}
-        margin={{
-          top: 20,
-          right: 20,
-          bottom: 20,
-          left: 20,
-        }}
-      >
-        <CartesianGrid />
-        <XAxis
-          type="number"
-          dataKey="x"
-          name="salary"
-          tickFormatter={formatCurrency}
-        >
-          <Label value="Annual salary" offset={0} position="bottom" />
-        </XAxis>
-        <YAxis
-          type="number"
-          dataKey="y"
-          name="years"
-          label={{
-            value: "Years of employment",
-            angle: -90,
-            position: "insideLeft",
+    <Card style={{ marginTop: "20px", marginLeft: "20px" }}>
+      <ResponsiveContainer height={400} width="95%" minWidth="375px">
+        <ScatterChart
+          height={400}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
           }}
-        />
-        <Tooltip
-          cursor={{ strokeDasharray: "3 3" }}
-          formatter={(value, name, props) =>
-            name === "salary" ? formatCurrency(value) : value
-          }
-        />
-        {highlightedEmployee && (
-          <Scatter
-            name="highlighted employee"
-            data={[highlightedData]}
-            fill="#b81010"
+        >
+          <CartesianGrid />
+          <XAxis
+            type="number"
+            dataKey="x"
+            name="salary"
+            tickFormatter={formatCurrency}
+          >
+            <Label value="Annual salary" offset={0} position="bottom" />
+          </XAxis>
+          <YAxis
+            type="number"
+            dataKey="y"
+            name="years"
+            label={{
+              value: "Years of employment",
+              angle: -90,
+              position: "insideLeft",
+            }}
           />
-        )}
-        <Scatter name="all employees" data={data} fill="#8884d8" />
-      </ScatterChart>
-    </ResponsiveContainer>
+          <Tooltip
+            cursor={{ strokeDasharray: "3 3" }}
+            formatter={(value, name, props) =>
+              name === "salary" ? formatCurrency(value) : value
+            }
+          />
+          {highlightedEmployee && (
+            <Scatter
+              name="highlighted employee"
+              data={[highlightedData]}
+              fill="#b81010"
+            />
+          )}
+          <Scatter name="all employees" data={data} fill="#8884d8" />
+        </ScatterChart>
+      </ResponsiveContainer>
+    </Card>
   );
 }
