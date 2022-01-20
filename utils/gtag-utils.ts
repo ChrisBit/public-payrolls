d59dcfed5608ef0d826c94e6d6851a65196509f8
dodgees <dodgees2@gmail.com>
2022-01-20 08:56:05 -0600

    Resolve new window problem

--- utils/gtag-utils.ts
+++ utils/gtag-utils.ts
@@ -1,15 +1,19 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export const pageview = (url) => {
-  // @ts-ignore
-  window.gtag("config", GA_TRACKING_ID, { page_path: url });
+  if (typeof window !== "undefined") {
+    // @ts-ignore
+    window.gtag("config", GA_TRACKING_ID, { page_path: url });
+  }
 };
 
 export const event = ({ action, category, label, value }) => {
-  // @ts-ignore
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
+  if (typeof window !== "undefined") {
+    // @ts-ignore
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
 };
